103303d80f732506bb0af56cb829fc94ed1e864c
dkhere <dengkehere@gmail.com>
2017-06-18 22:38:30 +0800

    add pre start show

--- CodingSpeed.py
+++ CodingSpeed.py
@@ -10,16 +10,28 @@ pygame.display.set_caption("Keyboard Demo")
 
 font1 = pygame.font.Font(None,24)
 font2 = pygame.font.Font(None,20)
+font3 = pygame.font.Font(None,40)
 white = 255,255,255
 yellow = 255,255,0
 correct_answer = random.randint(97,122)
 seconds = 11
 score = 0
 clock_start = 0
+is_start = False
 game_over = False
 def print_text(font, x, y, text, color=(255, 255, 255)):
     imgText = font.render(text, True, color)
     screen.blit(imgText, (x,y))
+
+def preStart(leftTime):
+    while leftTime != 0:
+        screen.fill((0, 100, 0))
+        pygame.draw.circle(screen,(155,155,155),(300,250),50,5)
+        print_text(font3,300-10,250-10,str(leftTime),(0,255,0))
+        pygame.display.update()
+        time.sleep(1)
+        leftTime = leftTime - 1;
+    
 
 while True:
     for event in pygame.event.get():
@@ -35,16 +47,20 @@ while True:
             clock_start = time.clock()
             game_over = False
             correct_answer = random.randint(97, 122)
-
+            is_start = False
+    screen.fill((0, 100, 0))
+    if is_start == False:
+        preStart(3)
+        is_start = True
     current = time.clock() - clock_start
-    speed = score * 60
+    speed = score * 6
     if seconds - current <= 0:
         game_over = True
     else:
         if keys[correct_answer]:
             correct_answer = random.randint(97,122)
             score += 1
-    screen.fill((0, 100, 0))
+
     print_text(font1, 0, 0, "Let`s see how fast you can type!")
     print_text(font2, 0, 20, "Try to keep up for 10 seconds...")
     print_text(font1, 500, 0, "Key: "+ chr(correct_answer))
